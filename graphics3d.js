29b4fba29713fdd9b8456a6d31440fba186ca6fe
hexahedria <hexahedria@gmail.com>
2016-02-20 15:14:47 -0800

    Lines

--- graphics3d.js
+++ graphics3d.js
@@ -4,6 +4,8 @@ var camera, controls, scene, renderer;
 
 var asschdron_geometry;
 var asschdron_colors;
+var asschdron_lines;
+var bkgd_dists;
 
 var asschdron_faces = [
     [5, 8, 11, 3, 13],
@@ -17,14 +19,17 @@ var asschdron_faces = [
     [8, 5, 10, 2],
 ];
 
+var used = {}
 var asschdron_edges = [];
 for (var i = 0; i < asschdron_faces.length; i++) {
     var face = asschdron_faces[i];
     for (var j = 0; j < face.length; j++) {
         var first = face[j];
         var second = face[(j+1)%face.length];
-        if(first<second){
-            asschdron_edges.push(first,second)
+        var key = Math.min(first,second) + 14*Math.max(first,second);
+        if(!used[key]){
+            used[key] = true;
+            asschdron_edges.push(first, second);
         }
     }
 }
@@ -32,7 +37,7 @@ for (var i = 0; i < asschdron_faces.length; i++) {
 function init_graphics3d(w,h,cvs) {
 
     camera = new THREE.PerspectiveCamera( 60, w / h, 0.001, 100000 );
-    camera.position.z = 500;
+    camera.position.z = 3;
 
     controls = new THREE.TrackballControls( camera, cvs );
 
@@ -56,10 +61,12 @@ function init_graphics3d(w,h,cvs) {
     scene.fog = new THREE.FogExp2( 0xffffff, 0*0.002 );
 
     asschdron_geometry = new THREE.Geometry();
+    asschdron_lines = new THREE.Geometry();
     asschdron_colors = [];
 
     for (var i = 0; i < 14; i++) {
-        asschdron_geometry.vertices.push(new THREE.Vector3(0,0,0));
+        var vec = new THREE.Vector3(0,0,0);
+        asschdron_geometry.vertices.push(vec);
         asschdron_colors.push(new THREE.Color(0xFFFFFF*i/13));
     }
 
@@ -76,10 +83,17 @@ function init_graphics3d(w,h,cvs) {
             asschdron_geometry.faces.push(tri);
         }
     }
-    //vertexColors: THREE.VertexColors,
-    var material =  new THREE.MeshBasicMaterial( { wireframe:false, vertexColors: THREE.VertexColors,  shading: THREE.FlatShading } );
 
-    var mesh = new THREE.Mesh( asschdron_geometry, material );
+    for (var i = 0; i < asschdron_edges.length; i++) {
+        var vert = asschdron_edges[i];
+        asschdron_lines.vertices.push(asschdron_geometry.vertices[vert]);
+    }
+    asschdron_lines.computeLineDistances();
+
+    //vertexColors: THREE.VertexColors,
+    var face_material =  new THREE.MeshBasicMaterial( { wireframe:false, vertexColors: THREE.VertexColors,  shading: THREE.FlatShading } );
+
+    var mesh = new THREE.Mesh( asschdron_geometry, face_material );
     mesh.position.x = 0;
     mesh.position.y = 0;
     mesh.position.z = 0;
@@ -87,13 +101,21 @@ function init_graphics3d(w,h,cvs) {
     mesh.matrixAutoUpdate = false;
     scene.add( mesh );
 
+    var line_material_back =  new THREE.LineDashedMaterial( { color: 0x000000, dashSize:.05, gapSize:.05, linewidth: 1, depthTest: false});
+    bkgd_dists = line_material_back;
+    var linesegs = new THREE.LineSegments(asschdron_lines,line_material_back);
+    scene.add(linesegs);
+
+    var line_material_front =  new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 2, depthTest: true});
+    var linesegs2 = new THREE.LineSegments(asschdron_lines,line_material_front);
+    scene.add(linesegs2);
     // lights
 
-    light = new THREE.DirectionalLight( 0xffff00 );
+    light = new THREE.DirectionalLight( 0xffffff );
     light.position.set( 1, 1, 1 );
     scene.add( light );
 
-    light = new THREE.DirectionalLight( 0x002288 );
+    light = new THREE.DirectionalLight( 0x333333 );
     light.position.set( -1, -1, -1 );
     scene.add( light );
 
@@ -116,27 +138,25 @@ function init_graphics3d(w,h,cvs) {
 
 function update_asschdron(points){
     var min_coord=Infinity, max_coord=-Infinity;
-    var totx=0,toty=0,totz=0;
     for (var i = 0; i < points.length; i++) {
         var pt = points[i];
-        totx += pt[0];
-        toty += pt[1];
-        totz += pt[2];
         min_coord = Math.min(min_coord, pt[3], pt[4], pt[5]);
         max_coord = Math.max(max_coord, pt[3], pt[4], pt[5]);
     }
-    var avgx=totx/points.length, 
-        avgy=toty/points.length,
-        avgz=totz/points.length;
     for (var i = 0; i < points.length; i++) {
         var pt = points[i];
-        asschdron_geometry.vertices[i].set((pt[0]-avgx)/100,(pt[1]-avgy)/100,(pt[2]-avgz)/100);
+        asschdron_geometry.vertices[i].set(pt[0],pt[1],pt[2]);
         asschdron_colors[i].setRGB( (pt[3]-min_coord)/(max_coord-min_coord),
                                     (pt[4]-min_coord)/(max_coord-min_coord),
                                     (pt[5]-min_coord)/(max_coord-min_coord));
     }
+    asschdron_geometry.normalize();
     asschdron_geometry.verticesNeedUpdate = true;
     asschdron_geometry.colorsNeedUpdate = true;
+    asschdron_lines.verticesNeedUpdate = true;
+    asschdron_lines.lineDistancesNeedUpdate = true;
+    asschdron_lines.computeLineDistances();
+    bkgd_dists.needsUpdate = true;
     render();
 }
 
